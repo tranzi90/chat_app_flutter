c89e97f88fd2aad0f3b18a65d0eea693200f9eca
denis_levchenko <tranzi90@gmail.com>
2023-08-03 23:35:04 +0300

    add my main notification function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Cloud Firestore triggers ref: https://firebase.google.com/docs/functions/firestore-events
+exports.myFunction = functions.firestore
+  .document("chat/{messageId}")
+  .onCreate((snapshot, context) => {
+    // Return this function's promise, so this ensures the firebase function
+    // will keep running, until the notification is scheduled.
+    return admin.messaging().sendToTopic("chat", {
+      // Sending a notification message.
+      notification: {
+        title: snapshot.data()["username"],
+        body: snapshot.data()["text"],
+        clickAction: "FLUTTER_NOTIFICATION_CLICK",
+      },
+    });
+  });
