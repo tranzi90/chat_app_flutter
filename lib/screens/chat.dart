29942a933a71b48a08884f31f3ea873adfd4da7f
denis_levchenko <tranzi90@gmail.com>
2023-08-04 23:39:53 +0300

    finish app

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,8 +1,9 @@
-import 'package:chat_app/widgets/chat_messages.dart';
-import 'package:chat_app/widgets/new_message.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
