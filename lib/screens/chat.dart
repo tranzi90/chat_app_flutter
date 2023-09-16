26a905c6ebccd2de9057ab5d645c92b669d3e98c
denis_levchenko <tranzi90@gmail.com>
2023-09-16 23:22:01 +0300

    const performance

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -45,8 +45,8 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
       ),
-      body: Column(
-        children: const [
+      body: const Column(
+        children: [
           Expanded(
             child: ChatMessages(),
           ),
